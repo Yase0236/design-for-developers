75bb4b0ccdbc66a151fbc9c41d764d1856b9330c
jasmin <yase0236@stud.kea.dk>
2022-10-11 17:28:22 +0200

    done with animations exercise

--- 13c.01.02.colors-and-animations/13c.01.02.colors-and-animations/script2.js
+++ 13c.01.02.colors-and-animations/13c.01.02.colors-and-animations/script2.js
@@ -1,27 +1,58 @@
-let theme = localStorage.getItem("data-theme");
+"use strict";
+import { inView, animate, scroll, stagger } from "https://cdn.skypack.dev/motion";
+
+document.addEventListener("DOMContentLoaded", checkTheme);
+/* ******************* DARK THEME *******************************/
 
 // Change theme to dark by adding the `dark` classname to html element.
-
 const changeThemeToDark = () => {
-  document.documentElement.setAttribute("data-theme", "dark"); //set theme to dark
+  document.documentElement.setAttribute("data-theme", "dark"); // set theme to dark
   localStorage.setItem("data-theme", "dark"); // save theme to local storage
 };
 
-// Reset the html class to default
-
 const changeThemeToLight = () => {
-  document.documentElement.setAttribute("data-theme", "light"); //set theme to light
-  localStorage.setItem("data-theme", "light"); // save theme to local storage
+  // set theme light
+  document.documentElement.setAttribute("data-theme", "light");
+  // save theme to local storage
+  localStorage.setItem("data-theme", "light");
 };
 
-// Get the element based on ID
-const checkbox = document.getElementById("switch");
+const checkbox = document.querySelector("#switch");
 // Apply retrived them to the website
 checkbox.addEventListener("change", () => {
-  let theme = localStorage.getItem("data-theme"); // Retrieve saved them from local storage
+  let theme = localStorage.getItem("data-theme");
+  // Retrieve saved them from local storage
   if (theme === "dark") {
     changeThemeToLight();
   } else {
     changeThemeToDark();
   }
+});
+
+function checkTheme() {
+  let theme = localStorage.getItem("data-theme");
+
+  if (theme === "dark") {
+    changeThemeToDark();
+    console.log(document.querySelector("#switch"));
+    //document.querySelector("input:checked + label");
+  } else {
+    changeThemeToLight();
+  }
+}
+
+/* scrolling animations */
+//PROGRESS BAR
+scroll(animate(".progress", { scaleY: [0, 1] }));
+
+// Selector
+inView(".highlight h2", (info) => {
+  console.log("highlight has enetered the view port");
+  const controls = animate(info.target, { opacity: [0, 1] }, { duration: 2 }, { repeat: 0 });
+});
+
+//animate articles
+inView(".article", (info) => {
+  console.log("article has enetered the view port");
+  const controls = animate(info.target, { opacity: [0, 1] }, { duration: 2 });
 });
